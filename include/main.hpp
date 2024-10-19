ea906471bae4b40163ca0b1357681b826807ab06
eduard <120855690+Eduard0110@users.noreply.github.com>
2024-10-19 18:50:09 +0100

    Add files via upload

---
+++ include/main.hpp
@@ -0,0 +1,13 @@
+﻿// SFMLphysics.h : Include file for standard system include files,
+// or project specific include files.
+
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "world.hpp"
+
+#endif
+// TODO: Reference additional headers your program requires here.
